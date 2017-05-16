4879a45009f9b2fb86b651f0c2e89df1c1d1a15f
chax <chackop@gmail.com>
2017-05-17 00:53:03 +0100

    bookroutes nav variable

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,46 +2,47 @@ var express = require('express');
 
 var bookRouter = express.Router();
 
-var books = [
-    {
-        title: 'The Odyssey',
-        genre: 'Greek',
-        author: 'Homer',
-        read: false
-    },
-    {
-        title: 'War and Peace',
-        genre: 'History',
-        author: 'Tolstoy',
-        read: false
-    },
-    {
-        title: 'Moby Dick',
-        genre: 'Greek',
-        author: 'Melville',
-        read: true
-    }
-];
+var router = function (nav) {
+    var books = [
+        {
+            title: 'The Odyssey',
+            genre: 'Greek',
+            author: 'Homer',
+            read: false
+        },
+        {
+            title: 'War and Peace',
+            genre: 'History',
+            author: 'Tolstoy',
+            read: false
+        },
+        {
+            title: 'Moby Dick',
+            genre: 'Greek',
+            author: 'Melville',
+            read: true
+        }
+    ];
 
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'hello from books',
-            nav: [
-                {
-                    Link: '/Books', Text: 'Books'
-                },
-                {
-                    Link: '/Authors', Text: 'Authors'
-                }
-            ],
-            books: books
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('bookListView', {
+                title: 'hello from books',
+                nav: nav,
+                books: books
+            });
         });
-    });
 
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello Single Book');
-    });
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render('bookView', {
+                title: 'hello from books',
+                nav: nav,
+                book: books[id]
+            });
+        });
 
-module.exports = bookRouter;
+    return bookRouter;
+};
+module.exports = router;
