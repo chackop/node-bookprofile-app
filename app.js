7e553826ab6f285221596571bfc34e9f87540d0a
chax <chackop@gmail.com>
2017-05-24 17:33:55 +0100

    passport setup

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -22,6 +26,11 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
